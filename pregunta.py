b825ba30610791d99a15fc941a80a40498da2439
sahernandezib <126585960+sahernandezib@users.noreply.github.com>
2023-06-08 19:55:05 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,33 +10,34 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 
 def ingest_data():
-    import re
 
     i = 0
-    dict_linea = {}
+    dict1 = {}
     df = pd.DataFrame()
     with open('./clusters_report.txt') as f:
         for line in f:
+            
 
             line = re.sub(r"\s+", " ", line)
             if len(line)>1 and i > 3:
                 if line.split()[0].isnumeric() == True:
                     try: 
-                        dict_linea['principales_palabras_clave'] = ' '.join(dict_linea['principales_palabras_clave'])
-                        df = df.append(dict_linea, ignore_index=True)
+                        dict1['principales_palabras_clave'] = ' '.join(dict1['principales_palabras_clave'])
+                        df = df.append(dict1, ignore_index=True)
                     except: pass
-                    dict_linea = {'cluster': int(line.split()[0]),
+                    dict1 = {'cluster': int(line.split()[0]),
                                 'cantidad_de_palabras_clave': int(line.split()[1]),
                                 'porcentaje_de_palabras_clave': float(line.split()[2].replace(',','.')),
                                 'principales_palabras_clave': line.split()[4:]}
                 else: 
-                    dict_linea['principales_palabras_clave'].append(' '.join(line.split()))
+                    dict1['principales_palabras_clave'].append(' '.join(line.split()))
             i += 1
-    dict_linea['principales_palabras_clave'] = ' '.join(dict_linea['principales_palabras_clave'])
-    df = df.append(dict_linea, ignore_index=True)
+    dict1['principales_palabras_clave'] = ' '.join(dict1['principales_palabras_clave'])
+    df = df.append(dict1, ignore_index=True)
     df['principales_palabras_clave'] = df['principales_palabras_clave'].str.rstrip('\.')
-    
+
     return df
